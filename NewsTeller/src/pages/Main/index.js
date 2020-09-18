27a29abb690fcc794fe3bf04d50a758cf0f11b95
Jonas Blanc <jonas.blanc@epfl.ch>
2020-09-18 16:18:41 +0200

    Add a fetch routine, to fetch news from local API (json-server)

--- NewsTeller/src/pages/Main/index.js
+++ NewsTeller/src/pages/Main/index.js
@@ -1,23 +1,47 @@
-import React from 'react';
-import {View, FlatList} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, FlatList, ActivityIndicator} from 'react-native';
 
 import NewsCard from '@components/NewsCard';
 
-const trumpArticles = require('@data/news_sample.json');
+//const trumpArticles = require('@data/news_sample.json');
 
 const _keyExtractor = (item, index) => item._id;
 
 export default Main = ({navigation}) => {
+  const [isLoading, setLoading] = useState(true);
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = () => {
+    return fetch('http://10.0.2.2:3002/response')
+      .then((response) => response.json())
+      .then((json) => {
+        setArticles(json.hits.hits);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <View>
-      <FlatList
-        data={trumpArticles.hits.hits}
-        keyExtractor={_keyExtractor}
-        numColumns={1}
-        renderItem={({item}) => (
-          <NewsCard navigation={navigation} newsItem={item} />
-        )}
-      />
+      {isLoading ? (
+        <ActivityIndicator size="small" color="#0000ff" />
+      ) : (
+        <View>
+          <FlatList
+            data={articles}
+            keyExtractor={_keyExtractor}
+            numColumns={1}
+            renderItem={({item}) => (
+              <NewsCard navigation={navigation} newsItem={item} />
+            )}
+          />
+        </View>
+      )}
     </View>
   );
 };
