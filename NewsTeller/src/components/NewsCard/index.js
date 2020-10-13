d80fe8808bdb9e93541a24386f1a0135ffb48d66
Jonas Blanc <jonas.blanc@epfl.ch>
2020-10-13 09:37:12 +0200

    Update NewsCard to navigate to WebDisplayer on click + comments

--- NewsTeller/src/components/NewsCard/index.js
+++ NewsTeller/src/components/NewsCard/index.js
@@ -1,30 +1,32 @@
-import React, {useEffect} from 'react';
-import {View, Text, Linking} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, Text} from 'react-native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 import {styles} from './style';
 
+/**
+ *  Display the article's title, source, publish date and number of reaction in card
+ *  The card is clickable: on click navigate to WebDisplayer page
+ */
 export default NewsCard = ({navigation, newsItem}) => {
-  var reaction = 0;
+  const [reaction, setReaction] = useState(0);
 
   useEffect(() => {
-    reaction =
+    // Count the number of reaction
+    setReaction(
       newsItem.favorite_count +
-      newsItem.quote_count +
-      newsItem.reply_count +
-      newsItem.retweet_count;
+        newsItem.quote_count +
+        newsItem.reply_count +
+        newsItem.retweet_count,
+    );
   }, []);
 
   return (
     <View style={styles.container}>
       <TouchableOpacity
-        onPress={
-          () => Linking.openURL(newsItem.url)
-          /*
-          navigation.navigate('Article', {
-            newsItem: newsItem,
-          })
-          */
-        }>
+        onPress={() => {
+          // Navigate to WebDisplayer when the user clicks on the card
+          navigation.navigate('WebDisplayer', {url: newsItem.url});
+        }}>
         <Text style={styles.title}>{newsItem.title}</Text>
         <View style={styles.metaDataBox}>
           <Text style={[styles.text, styles.metaData]}>{newsItem.handle}</Text>
